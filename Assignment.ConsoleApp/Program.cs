490f24a007dca579b3fce57aef0df1f361a7912e
Bojan Boshkoski <bojan.boskoski@gmail.com>
2022-02-18 17:59:55 +0100

    Init...

---
+++ Assignment.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
